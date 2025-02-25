f30bc598878b345032e7aba47189288fdbd59082
Patti <panderson@southern.edu>
2025-02-25 17:36:47 -0500

    Removed unnecessary comments

--- RootsApp/RootsApp/Messages.cs
+++ RootsApp/RootsApp/Messages.cs
@@ -1,19 +1,16 @@
 ﻿namespace RootsApp
 {
-    //public class Errors
-    //{
-    //    public enum ROOT_ERRORS { NONE, SAME_SIGN, DIVISION_BY_ZERO, RUNAWAY, CYCLE };
-    //}
+    
     public class Messages
     {
 
         public enum ROOT_ERRORS { NONE, SAME_SIGN, DIVISION_BY_ZERO, RUNAWAY, CYCLE };
         public static void DivisionByZeroError(RootObj r)
         {
-            //Root r = new Root();
+            
             r.root = double.NaN;
             r.error = (int)ROOT_ERRORS.DIVISION_BY_ZERO;
-            //return r;
+            
         }
 
         public static void SameSign(RootObj r)
@@ -26,18 +23,18 @@
 
         public static void RunawayError(RootObj r)
         {
-            //Root r = new Root();
+            
             r.root = double.NaN;
             r.error = (int)ROOT_ERRORS.RUNAWAY;
-            //return r;
+            
         }
 
         public static void CycleError(RootObj r)
         {
-            //Root r = new Root();
+           
             r.root = double.NaN;
             r.error = (int)ROOT_ERRORS.CYCLE;
-            //return r;
+            
         }
     }
 }
